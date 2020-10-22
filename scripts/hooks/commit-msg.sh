8c2e3f9a7751c213d58f9bfdd7c75d9624e04444
Devlin Junker <devlin.junker@gmail.com>
2020-10-21 22:55:32 -0700

    next-version: 0.9.0

--- scripts/hooks/commit-msg.sh
+++ scripts/hooks/commit-msg.sh
@@ -37,3 +37,10 @@ main() {
 
 main $1
 
+COMMIT_MSG=`cat $1 | sed -n "/^[^#]/p"`
+
+COMMIT_WORD_COUNT=`cat $1 | sed -n "/^[^#]/p" | wc -w | sed "s/\w//"`
+COMMIT_LINE_COUNT=`cat $1 | sed -n "/^[^#]/p" | wc -l | sed "s/\w//"`
+
+echo $COMMIT_WORD_COUNT
+echo $COMMIT_LINE_COUNT
