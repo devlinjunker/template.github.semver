8e9edd9fb010b2ab7b023ff6c2ceaecc96afba18
Devlin Junker <devlin.junker@gmail.com>
2020-10-21 21:27:49 -0700

    started branch-protections.sh that gets git info for now

---
+++ scripts/hooks/branch-protections.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+# Script to check github branch protections and prevent commits to protected branches
+
+BLOCKED_BRANCH=( master )
+BLOCKED_PREFIC=( release )
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+github() {
+
+  local REMOTE=`git status -sb | sed -n "/##/p" | sed "s/.*\.\.\.//" | sed "s/\/.*//"`
+
+  if [[ ! $REMOTE =~ "##" ]]; then
+    local URL=`git config --get remote."$REMOTE".url`
+    
+    if [[  $URL =~ "github.com" ]]; then
+      local SPLIT=(`echo $URL | sed "s/.*://" | sed  "s/\.git$//" | sed "s/\// /" `)
+      local OWNER=${SPLIT[0]}
+      local REPO=${SPLIT[1]}
+
+      #echo $OWNER
+      #echo $REPO
+    else
+      echo "not a github remote"
+    fi
+  else
+    echo "no upstream branch"
+  fi
+
+  echo $BRANCH
+
+  # Note: Requires github token... so idk...
+  #AUTH="token "
+  # curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: $AUTH" "https://api.github.com/repos/$OWNER/$REPO/branches/$BRANCH/protection"
+
+}
+
+main() {
+  # check github branch protections
+  github
+  
+  # compare against branch name/prefixes defined in here
+  # if BLOCKED_BRANCH matches
+  # or if branch starts with BLOCKED_PREFIX
+  # return -1
+   
+}
+main
+
+
