ddf258370e460f0d9dd7e40a5ad3d595c9d6df72
Devlin Junker <devlin.junker@gmail.com>
2020-10-21 22:55:32 -0700

    next-version: 0.9.0

--- scripts/hooks/commit-msg.sh
+++ scripts/hooks/commit-msg.sh
@@ -2,3 +2,10 @@
 # Script to verify that commit message matches conventions
 
 
+COMMIT_MSG=`cat $1 | sed -n "/^[^#]/p"`
+
+COMMIT_WORD_COUNT=`cat $1 | sed -n "/^[^#]/p" | wc -w | sed "s/\w//"`
+COMMIT_LINE_COUNT=`cat $1 | sed -n "/^[^#]/p" | wc -l | sed "s/\w//"`
+
+echo $COMMIT_WORD_COUNT
+echo $COMMIT_LINE_COUNT
