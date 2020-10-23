1279bf3e3cd4768d12e6cba4458019504e5fabcc
Devlin Junker <devlin.junker@gmail.com>
2020-10-23 09:34:23 -0700

    add pre-push hook to prevent branches with invalid names being pushed

--- scripts/hooks/README.md
+++ scripts/hooks/README.md
@@ -5,4 +5,6 @@ Git Hooks are run by Git during specific actions
 ## To Add to Workflow
 Git hooks need to be added to your local repo for each project,
 - Create Symbolic links in `.git/hooks/` for git hook scripts you want to use
-  e.g. `cd .git/hooks/; ln -s ../../scripts/hooks/pre-commit.sh pre-commit`
+  1. `cd .git/hooks/`
+  2. `ln -s ../../scripts/hooks/pre-commit.sh pre-commit`
+  3. `ln -s ../../scripts/hooks/pre-push.sh pre-push`
---
+++ scripts/hooks/pre-push.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+
+main() {
+  local DIR=`dirname "$0"`
+
+  # if in hook, then prep PATH to find in repo `scripts/hooks/` dir
+  if [[ $DIR =~ ".git" ]]; then
+    DIR+="/../../scripts/hooks"
+  fi
+
+
+  local BRANCH_NAME_ERROR=" !  Branch name does not match conventions  "
+
+  # Call branch name script
+  $DIR/branch-name.sh
+  if [ $? -ne 0 ]; then
+    echo "$(tput setaf 1)$(tput setab 7)$BRANCH_NAME_ERROR$(tput sgr 0)"
+    echo "  <prefix>/<description>"
+    echo "prefix options: ($($DIR/branch-name.sh -o))"
+    return -1
+  fi
+}
+
+main
