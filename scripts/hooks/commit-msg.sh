65f35d1774ef1e4976a3271edaba711630d010ea
Devlin Junker <devlin.junker@gmail.com>
2020-11-02 21:06:17 -0800

    feat: commit-msg hook checks first line

--- scripts/hooks/commit-msg.sh
+++ scripts/hooks/commit-msg.sh
@@ -1,20 +1,6 @@
 #! /bin/bash
 # Script to verify that commit message matches conventions
 
-COMMIT_WORD_COUNT=`cat $1 | sed -n "/^[^#]/p" | wc -w | sed "s/\w//"`
-COMMIT_LINE_COUNT=`cat $1 | sed -n "/^[^#]/p" | wc -l | sed "s/\w//"`
-# echo $COMMIT_WORD_COUNT
-# echo $COMMIT_LINE_COUNT
-
-
-MSG_PREFIXES=( 'wip' )
-
-
-COMMIT_MSG=`cat $1 | sed -n "/^[^#]/p"`
-FIRST_LINE=`echo $COMMIT_MSG | sed -n "/[^\w]*$/p" | sed "s/^ *//" | sed "s/\n//" | head`
-
-COMMIT_MSG_ERROR=" ! Invalid commit message  "
-
 DIR=`dirname "$0"`
 # if in hook, then prep PATH to find in repo `scripts/hooks/` dir 
 if [[ $DIR =~ ".git" ]]; then
@@ -22,18 +8,33 @@ if [[ $DIR =~ ".git" ]]; then
 fi
 
 
-# get prefix options from shared prefix list file
-PREFIXES=`$DIR/prefix-list.sh`
-
-# create regexp for first line of commit message:      <prefix>(optional !):<description>
-regexp="^($(echo "${PREFIXES[@]} ${MSG_PREFIXES[@]}" | sed "s/ /|/g"))(\([\w\s]*\))?\!?\:[A-Za-z0-9\._\-\s]*"
+MSG_PREFIXES=( 'wip' )
+OTHER_PREFIXES=`$DIR/prefix-list.sh`
 
 
-# check that first line of message matches regexp
-if [[ ! $FIRST_LINE =~ $regexp ]]; then
-  echo "$(tput setaf 1)$(tput setab 7)$COMMIT_MSG_ERROR$(tput sgr 0)"
-  echo "  <prefix>:<description>"
-  echo "prefix options: ($PREFIXES $MSG_PREFIXES)"
-  exit -1
-fi
+COMMIT_MSG_ERROR=" ! Invalid commit message  "
+
+main() {
+  COMMIT_MSG=`cat $1 | sed -n "/^[^#]/p"`
+  FIRST_LINE=`echo $COMMIT_MSG | sed -n "/[^\w]*$/p" | sed "s/^ *//" | sed "s/\n//" | head`
+
+  # create regexp for first line of commit message:      <prefix>(optional !):<description>
+  regexp="^($(echo "${MSG_PREFIXES[@]} ${OTHER_PREFIXES[@]}" | sed "s/ /|/g"))(\([\w\s]*\))?\!?\:[A-Za-z0-9\._\-\s]*"
+
+  # check that first line of message matches regexp
+  if [[ ! $FIRST_LINE =~ $regexp ]]; then
+    echo "$(tput setaf 1)$(tput setab 7)$COMMIT_MSG_ERROR$(tput sgr 0)"
+    echo "  <prefix>:<description>"
+    echo "prefix options: ($OTHER_PREFIXES $MSG_PREFIXES)"
+    exit -1
+  fi
+}
+
+main $1
+
+
+
+
+
+
 
