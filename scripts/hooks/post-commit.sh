d58990d9474bb8f5bca67e2b5bbec77356a84c06
Devlin Junker <devlin.junker@gmail.com>
2020-11-04 22:29:04 -0800

    feature: post-commit hook complete with warnings

--- scripts/hooks/post-commit.sh
+++ scripts/hooks/post-commit.sh
@@ -1,27 +1,53 @@
 #! /bin/bash
 # Checks that the git diff is not getting to big and warns if it is
 
-# TODO: how can we determine the branch we want to check against?
 
-# last commit with "(#<number>)" in commit message
-DIFF_BRANCH=`git log --pretty=oneline | sed -n "/\(#[0-9]*\)/p" | head -1 | sed "s/ .*//"`
+# TODO: better way to determine the branch/commit we want to check against?
+# for now look back until DIFF_BRANCH and find other merge commits -- those with (#..) in the commit message
+#   if none found, use DIFF_BRANCH
+# NOTE: This requires enforcing merge messages
 
-# DIFF_BRANCH=master
+DIFF_BRANCH=master
+
+# find last commit with "(#<number>)" in commit message
+LAST_MERGE=`git log $DIFF_BRANCH..HEAD --pretty=oneline | sed -n "/\(#[0-9]*\)/p" | head -1 | sed "s/ .*//"`
+
+# if not exists since $DIFF_BRANCH then just use $DIFF_BRANCH
+if [ -z "$LAST_MERGE" ]; then
+  DIFF_COMMIT=master
+else
+  DIFF_COMMIT=$LAST_MERGE
+fi
+
+
 
 FILE_LIMIT=10
 LINE_LIMIT=1000
 
+FILE_LIMIT_WARNING=" ! warning: more than $FILE_LIMIT files changed since $DIFF_COMMIT "
+LINE_LIMIT_WARNING=" ! warning: more than $LINE_LIMIT files changed since $DIFF_COMMIT "
+NEW_BRANCH_CMDS="Create new branch with 'git checkout -b <branch_name>' then push to remote with 'git push --set-upstream <remote_name> <branch_name>'"
 main() {
-  DIFF_FILES=`git diff HEAD..$DIFF_BRANCH --stat | tail -n1 | tr ' ' '\n' | sed -n "/[[:digit:]]/p" | head -1`
-  DIFF_LINES=`git diff HEAD..$DIFF_BRANCH --stat | tail -n1 | tr ' ' '\n' | sed -n "/[[:digit:]]/p" | tail -1`
+  DIFF_FILES=`git diff HEAD..$DIFF_COMMIT --stat | tail -n1 | tr ' ' '\n' | sed -n "/[[:digit:]]/p" | head -1`
+  DIFF_LINES=`git diff HEAD..$DIFF_COMMIT --stat | tail -n1 | tr ' ' '\n' | sed -n "/[[:digit:]]/p" | tail -1`
 
+  WARN=false
   if [ $DIFF_FILES -gt $FILE_LIMIT ]; then
-    echo " ! warning: more than $FILE_LIMIT files changed since $DIFF_BRANCH : $DIFF_FILES"
+    echo "$(tput setaf 3)$FILE_LIMIT_WARNING$(tput sgr 0)" 
+    WARN=true
   fi
 
   if [ $DIFF_LINES -gt $LINE_LIMIT ]; then
-    echo " ! warning: more than $LINE_LIMIT files changed since $DIFF_BRANCH : $DIFF_LINES"
+    echo "$(tput setaf 3)$LINE_LIMIT_WARNING$(tput sgr 0)"
+    WARN=true
   fi
+  echo "Unmerged Files: $DIFF_FILES    Lines: $DIFF_LINES
+"  
+
+  if [ $WARN = true ]; then
+    echo $NEW_BRANCH_CMDS
+  fi
+
 }
 
 main
