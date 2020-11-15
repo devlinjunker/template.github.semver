ceda3ec23708996ca117149ebb82905a39f14e24
Devlin Junker <devlin.junker@gmail.com>
2020-11-14 20:35:04 -0800

    feature:prevent wip commits

--- scripts/actions/verify-merge.sh
+++ scripts/actions/verify-merge.sh
@@ -3,11 +3,13 @@
 
 echo "$BRANCH"
 if [[ "$BRANCH" =~ "poc/" ]]; then
+  echo "Not allowed to merge POC branch"
   exit -1 
 fi;
 
 MSG=`git log $BRANCH -1 --pretty=format:"%s"`
 echo "$MSG"
-if [[ "$MSG" =~ wip* ]]; then
+if [[ "$MSG" = wip* ]]; then
+  echo "Not allowed to merge when last commit message starts with 'wip'"
   exit -1
 fi
