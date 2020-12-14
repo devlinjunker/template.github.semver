6efd24a0a33c547a29bc8e370212f3e8cb9d1d8f
Devlin Junker <devlin.junker@gmail.com>
2020-12-13 17:50:31 -0800

    doc: clean up notes in script file

--- scripts/workflows/collect-wiki.sh
+++ scripts/workflows/collect-wiki.sh
@@ -1,21 +1,19 @@
 #! /bin/bash
 
-# 1. create `wiki/` directory
 mkdir wiki
 
-# 2. find all README files
 FILES=$(find . -name *README* | tail -n +2);
 
 WIKI_FILES=();
-# 3. rename each `_README_<directory>` and place in `wiki/` directory 
 for f in $FILES
 do
+    # rename each to `_<directory>_README` and place in `wiki/` directory 
     wiki_name=$(echo $f | sed "s/^\.\///" | sed "s/^\.//" | sed "s/^/_/" | sed "s/\//_/g")
     WIKI_FILES+=("$wiki_name")
     cp $f wiki/$wiki_name
 done
 
-# 4. create `.README` file with Table of Contents to link to each
+# create `.README` file with Table of Contents to link to each
 echo "These pages are copied from from .md files in the source code. These will be defined in a configuration file." > wiki/.README.md
 echo "" >> wiki/.README.md
 echo "Each page name that is synced will have name: _README_<file_name> so it is not visible in the 'Page List' and will instead be linked below. These files **should not be modified** because they will be overwritten on each merge to develop." >> wiki/.README.md
@@ -25,7 +23,7 @@ echo ""
 
 for f in ${FILES[@]}
 do
-## create row with each wiki file copied
+    ## create TOC row with each wiki file copied
     link=$(echo $f | sed "s/^\.\///" | sed "s/^\.//" | sed "s/^/_/" | sed "s/\//_/g" | sed "s/\.md$//")
-	echo "- [$f]($link)" >> wiki/.README.md
+    echo "- [$f]($link)" >> wiki/.README.md
 done
