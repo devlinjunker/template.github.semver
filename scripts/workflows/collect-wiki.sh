9192566973eb4502b058ded05b4b739177029356
Devlin Junker <devlin.junker@gmail.com>
2020-12-14 19:29:21 -0800

    fix: wiki sync action does not include README, also includes CONTRIBUTING and SECURIT Y

--- scripts/workflows/collect-wiki.sh
+++ scripts/workflows/collect-wiki.sh
@@ -2,17 +2,6 @@
 
 mkdir wiki
 
-FILES=$(find . -name *README* | tail -n +2);
-
-WIKI_FILES=();
-for f in $FILES
-do
-    # rename each to `_<directory>_README` and place in `wiki/` directory 
-    wiki_name=$(echo $f | sed "s/^\.\///" | sed "s/^\.//" | sed "s/^/_/" | sed "s/\//_/g")
-    WIKI_FILES+=("$wiki_name")
-    cp $f wiki/$wiki_name
-done
-
 # create `.README` file with Table of Contents to link to each
 echo "These pages are copied from from .md files in the source code. These will be defined in a configuration file." > wiki/.README.md
 echo "" >> wiki/.README.md
@@ -20,9 +9,30 @@ echo "Each page name that is synced will have name: _README_<file_name> so it is
 echo "" >> wiki/.README.md
 echo "**Docs**" >> wiki/.README.md
 
-for f in ${FILES[@]}
+OTHER_FILES=("./CONTRIBUTING.md" "./SECURITY.md")
+
+
+for f in "${OTHER_FILES[@]}"
 do
-    ## create TOC row with each wiki file copied
+    echo $f;
+    # rename each to `_<directory>_README` and place in `wiki/` directory 
+    wiki_name=$(echo $f | sed "s/^\.\///" | sed "s/^\.//" | sed "s/^/_/" | sed "s/\//_/g")_README
+    cp $f wiki/$wiki_name
+    link=$(echo $f | sed "s/^\.\///" | sed "s/^\.//" | sed "s/^/_/" | sed "s/\//_/g" | sed "s/\.md$//")_README
+    echo "- [$f]($link)" >> wiki/.README.md
+done
+
+
+README_FILES=$( find . -name *README* );
+
+for f in ${README_FILES[@]}
+do
+    if [ "$f" = "./README.md" ]; then continue; fi
+    echo $f;
+    # rename each to `_<directory>_README` and place in `wiki/` directory 
+    wiki_name=$(echo $f | sed "s/^\.\///" | sed "s/^\.//" | sed "s/^/_/" | sed "s/\//_/g")
+    cp $f wiki/$wiki_name
+
     link=$(echo $f | sed "s/^\.\///" | sed "s/^\.//" | sed "s/^/_/" | sed "s/\//_/g" | sed "s/\.md$//")
     echo "- [$f]($link)" >> wiki/.README.md
 done
