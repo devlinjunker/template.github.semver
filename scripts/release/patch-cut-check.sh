e9a51c4661a6a4dbd55f65721af8dcc686124061
Devlin Junker <devlin.junker@gmail.com>
2021-04-04 18:51:26 -0700

    fix: use glob instead of regexp

--- scripts/release/patch-cut-check.sh
+++ scripts/release/patch-cut-check.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if ! [[ "$1" =~ "main|release-*" ]]; then
+if ! [[ "$1" != "origin/main" && "$1" != "origin/release-*" ]]; then
   echo "::error::Cannot Cut Patch off of non 'release-*' or 'main' branch";
   exit 1        
 fi;
