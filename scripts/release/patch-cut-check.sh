48ef35c7b87a9474fbaa66946d4db0042eaca991
Devlin Junker <devlin.junker@gmail.com>
2021-04-04 19:23:19 -0700

    fix: fix patch-cut-check script call

--- scripts/release/patch-cut-check.sh
+++ scripts/release/patch-cut-check.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if ! [[ "$1" != "origin/main" && "$1" != "origin/release-*" ]]; then
+if [[ "$1" != "main" && "$1" != "release-*" ]]; then
   echo "::error::Cannot Cut Patch off of non 'release-*' or 'main' branch";
   exit 1        
 fi;
